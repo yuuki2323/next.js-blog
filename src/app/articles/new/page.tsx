4ea098e0fb759b522954eeb7cc6435a58385ed54
yuuki2323 <yuuki.work23@gmail.com>
2023-10-03 12:41:51 +0900

    third

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -1,15 +1,31 @@
+"use client";
+import React, { useState } from "react";
+import { create } from "domain";
 import React from "react";
 
 const CreateBlogPage = () => {
+
+  
+  const [id,setId] = useState<string>("");
+  const [title,setTitle] = useState<string>("");
+  const [content,setContent] = useState<string>("");
+
+const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
+  e.preventDefault();
+
+  await createArticle(id,title,content);
+};
+
   return (
     <div className="min-h-screen py-8 px-4 md:px-12">
       <h2 className="text-2xl font-bold mb-4">ブログ新規作成</h2>
-      <form className="bg-slate-200 p-6 rounded shadow-lg">
+      <form className="bg-slate-200 p-6 rounded shadow-lg" onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="text-gray-700 text-sm font-bold mb-2">URL</label>
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focas: outline-none"
+            onChange={(e)=> setId(e.target.value)}
           />
           </div>
           <div className="mb-4">
@@ -18,7 +34,8 @@ const CreateBlogPage = () => {
           </label>
           <input
             type="text"
-            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focas: outline-none"
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus: outline-none"
+            onChange={(e)=> setTitle(e.target.value)}
           />
           </div>
           <div className="mb-4">
@@ -27,6 +44,7 @@ const CreateBlogPage = () => {
             </label>
           <textarea
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focas: outline-none"
+            onChange={(e)=> setContent(e.target.value)}
           />
           </div>
     
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -26,3 +26,28 @@ await new Promise((resolve) => setTimeout(resolve,1000));
     const article  = await res.json();
     return article;
 };
+
+export const createArticle = async (
+    id: string,
+     title:string,
+     content:string
+    ) : Promise<Article> => {
+
+        const currentDatetime=new Date().toISOString();
+
+    const res =  await fetch(`http://localhost:3001/posts`, {
+        method: "POST",
+        headers:{"Content-Type":"application/json",
+    },
+    body:JSON.stringify({id,title,content,contentAt:currentDatetime})
+
+});
+    
+
+    if(!res.ok){
+        throw new Error('エラーが発生しました。');
+    }
+    await new Promise((resolve) => setTimeout(resolve,1000));
+        const newArticle  = await res.json();
+        return newArticle;
+    };
